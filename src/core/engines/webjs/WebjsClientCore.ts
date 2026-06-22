1db8ae3423804cc536cda122b138503189e736c2
devlikepro <waha@devlike.pro>
2026-06-22 14:30:55 +0700

    [core] Merge PLUS functionality into CORE
    Copy the storage (Mongo/Postgres/SQLite), media (S3/Postgres), engine
    auth/store and util implementations from src/plus into src/core, and fold
    the *Plus engine sessions, session manager, factories, health and channels
    services into their *Core classes.
    
    Core now supports multi-session orchestration, Mongo/Postgres session
    storage, S3/Postgres media storage, FFmpeg media conversion, real health
    checks and channels metadata. mediaConverter is set once in the base
    session, and AppModuleCore wires the conditional media modules and health
    indicators.

--- src/core/engines/webjs/WebjsClientCore.ts
+++ src/core/engines/webjs/WebjsClientCore.ts
@@ -8,8 +8,9 @@ import { sleep } from '@waha/utils/promiseTimeout';
 import { EventEmitter } from 'events';
 import * as lodash from 'lodash';
 import { Page } from 'puppeteer';
-import { Client, Events } from 'whatsapp-web.js';
+import { Client, Events, Message as WebjsMessage } from 'whatsapp-web.js';
 import { Message } from 'whatsapp-web.js/src/structures';
+import { Message as MessageInstance } from 'whatsapp-web.js/src/structures';
 
 import { CallErrorEvent, PAGE_CALL_ERROR_EVENT, WPage } from './WPage';
 
@@ -23,12 +24,76 @@ const { LoadLodash } = require('./_lodash.js');
 const { LoadPaginator } = require('./_Paginator.js');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { LoadWAHACore } = require('./_WAHACore.js');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const ChatFactory = require('whatsapp-web.js/src/factories/ChatFactory');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {
   exposeFunctionIfAbsent,
 } = require('whatsapp-web.js/src/util/Puppeteer');
+
+export interface WebjsChannelMessage {
+  message: WebjsMessage;
+  reactions: ChannelMessageReaction[];
+  viewCount: number;
+}
+
+class ChannelMessageReaction {
+  reaction: string;
+  count: number;
+}
+
+interface _Id {
+  id: string;
+  fromMe: boolean;
+  remote: string;
+  _serialized: string;
+}
+
+/**
+ *     "parentMsgKey": {
+ *         "fromMe": false,
+ *         "remote": "111111111111111111@newsletter",
+ *         "id": "AAAAAAAAAAAAAAAAAAAA",
+ *         "_serialized": "false_111111111111111111@newsletter_AAAAAAAAAAAAAAAAAAAA"
+ *     },
+ *     "serverTimestamp": 1738536731,
+ *     "emojiCountMap": {emoji=>count}
+ */
+interface _NewsletterReaction {
+  parentMsgKey: _Id;
+  serverTimestamp: number;
+  emojiCountMap: any;
+}
+
+interface _GetNewsletterPreviewDataResponse {
+  ids: any[];
+  newsletterMetadata: any;
+  newsletterMessages: any[];
+  newsletterReactions: _NewsletterReaction[];
+  timestamp: number;
+}
+
+function extractReactionsByMessageKey(
+  newsletterReactions: _NewsletterReaction[],
+): Map<string, ChannelMessageReaction[]> {
+  const reactions = new Map();
+  for (const reaction of newsletterReactions) {
+    const key = reaction.parentMsgKey._serialized;
+    const emojiCountMap = reaction.emojiCountMap;
+    const reactionList: ChannelMessageReaction[] = [];
+    for (const emoji in emojiCountMap) {
+      reactionList.push({
+        reaction: emoji,
+        count: emojiCountMap[emoji],
+      });
+    }
+    reactions.set(key, reactionList);
+  }
+  return reactions;
+}
 
 export class WebjsClientCore extends Client {
   public events = new EventEmitter();
@@ -66,6 +131,7 @@ export class WebjsClientCore extends Client {
     await this.pupPage.evaluate(LoadLodash);
     await this.pupPage.evaluate(LoadPaginator);
     await this.pupPage.evaluate(LoadWAHA);
+    await this.pupPage.evaluate(LoadWAHACore);
   }
 
   /**
@@ -577,5 +643,101 @@ export class WebjsClientCore extends Client {
     await this.subscribePresence(chatId);
     await sleep(3_000);
     return await this.getCurrentPresence(chatId);
+  }
+
+  /**
+   * Channels methods
+   */
+  async channelFetchMessageByInvite(
+    inviteCode: string,
+    limit: number,
+  ): Promise<WebjsChannelMessage[]> {
+    await this.ensureWahaExtraInjected();
+    const response: _GetNewsletterPreviewDataResponse =
+      await this.pupPage.evaluate(
+        async (code, limit) => {
+          // Overwrite limit
+          window['WAHA'].WAWebNewsletterGatingUtils = () => limit;
+
+          const result = await window[
+            'WAHA'
+          ].WAWebNewsletterPreviewJob.getNewsletterPreviewData(code, 'guest');
+          for (const newsletterReaction of result.newsletterReactions) {
+            // puppeter doesn't support Map,
+            // so we need to convert it to object
+            newsletterReaction.emojiCountMap = Object.fromEntries(
+              newsletterReaction.emojiCountMap,
+            );
+          }
+
+          // Fetch one more time to save in database so we can fetch media later
+          await window[
+            'WAHA'
+          ].WAWebLoadNewsletterPreviewChatAction.loadNewsletterPreviewChat(
+            code,
+          );
+          return result;
+        },
+        inviteCode,
+        limit,
+      );
+    const messageInstances = response.newsletterMessages
+      .filter((msg) => msg.type != 'revoked')
+      .map((msg) => {
+        return new MessageInstance(this, msg);
+      });
+    const reactions = extractReactionsByMessageKey(
+      response.newsletterReactions,
+    );
+
+    const messages: WebjsChannelMessage[] = messageInstances.map((msg) => {
+      return {
+        message: msg,
+        reactions: reactions.get(msg.id._serialized) || [],
+        viewCount: msg.rawData.viewCount,
+      };
+    });
+    return messages;
+  }
+
+  /**
+   * Channels Search methods
+   */
+  async searchChannelsView(params: any): Promise<any> {
+    await this.ensureWahaExtraInjected();
+    const newsletters: any = await this.pupPage.evaluate(async (params) => {
+      return await window[
+        'WAHA'
+      ].WAWebNewsletterDirectorySearchJob.getNewsletterDirectoryList(params);
+    }, params);
+    return newsletters;
+  }
+
+  async searchChannelsText(params: any): Promise<any> {
+    await this.ensureWahaExtraInjected();
+    const newsletters: any = await this.pupPage.evaluate(async (params) => {
+      return await window[
+        'WAHA'
+      ].WAWebNewsletterDirectorySearchJob.getNewsletterDirectorySearchResults(
+        params,
+      );
+    }, params);
+    return newsletters;
+  }
+
+  protected async ensureWahaExtraInjected() {
+    const hasWahaExtra = await this.pupPage.evaluate(() => {
+      return Boolean(
+        // @ts-ignore
+        window.WAHA &&
+          // @ts-ignore
+          window.WAHA.WAWebNewsletterDirectorySearchJob &&
+          // @ts-ignore
+          window.WAHA.WAWebNewsletterPreviewJob,
+      );
+    });
+    if (!hasWahaExtra) {
+      await this.injectWaha();
+    }
   }
 }
